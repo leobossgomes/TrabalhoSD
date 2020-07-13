7741426c917a4b3d701f9d4563f5ae8a9fdb9cbf
leobossgomes <30898226+leobossgomes@users.noreply.github.com>
2020-07-12 21:07:32 -0300

    Add files via upload

---
+++ ApiRobo/Models/PontoEntrega.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterfacexD.Models {
+    public class PontoEntrega : Objeto {
+        public override string RepresentacaoVisual { get { return "P"; } }
+
+        public PontoEntrega(int id, int posicaoX, int posicaoY) : base(id, posicaoX, posicaoY) { }
+
+        protected override void Mover() {
+            throw new Exception("O galpão não pode ser movido!");
+        }
+    }
+}
